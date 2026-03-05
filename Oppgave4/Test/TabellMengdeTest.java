78cdb9ef3016f534a962750ee58ed6815380d330
Norgisk <danieldjuvstad@gmail.com>
2026-03-05 09:47:14 +0100

    Lagde skallkode for Junit tester
    Lagt inn skallkode for Junit, laget dependency til jUnit 5. Lå inn parametere på antallElementer for å ikke få feilmelding.

--- Oppgave4/Test/TabellMengdeTest.java
+++ Oppgave4/Test/TabellMengdeTest.java
@@ -1,5 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 public class TabellMengdeTest {
 
+    @BeforeEach
+    public void setUp() {
+        
+    }
 
+    @Test
+    void erTom() {
+    }
 
+    @Test
+    void inneholder() {
+    }
+
+    @Test
+    void erDelmengdeAv() {
+    }
+
+    @Test
+    void erLik() {
+    }
+
+    @Test
+    void erDisjunkt() {
+    }
+
+    @Test
+    void snitt() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void leggTil() {
+    }
+
+    @Test
+    void leggTilAlleFra() {
+    }
+
+    @Test
+    void fjern() {
+    }
+
+    @Test
+    void tilTabell() {
+    }
+
+    @Test
+    void antallElementer() {
+    }
 }
